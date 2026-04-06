0269a432c4678c431829ae5ddb67ddbbb2443f98
Ding <582261456@qq.com>
2026-04-06 12:12:01 +0800

    first commit!

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time: 2026/4/6 11:36
+# @Author: TomDing
+
+"""
+创建虚拟环境
+    python -m venv .venv
+
+激活虚拟环境
+    source .venv/bin/activate
+
+编辑配置文件
+    edit pyproject.toml
+
+同步配置
+    pip install -e .
+"""
+from flask import flash
+import requests
+from lxml import etree
+
+
+
+
+
